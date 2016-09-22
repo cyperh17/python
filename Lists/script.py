57d5dcfc73826fbc5ccbb00d9697f68611eaac48
cyperh17 <cyperh17@gmail.com>
2016-09-22 09:37:55 +0300

    added lists

---
+++ Lists/script.py
@@ -0,0 +1,40 @@
+import functools
+
+#l = range(0, 10)
+
+#print(l[0]) # element at index 0
+#print(l[-10]) # element at index 0
+
+#-1 - last item in list
+#-10 - first item
+
+#print(list(l[0:5])) # in operator, prints every value from idex 0 to 4, list converts the result to the list
+
+#print(list(l[3:])) # prints every value from idex 0 to the end
+
+#print(list(l[0:10:2])) # prints every value from 0 to 9 with step 2
+#print(list(l[-1:2:-1])) # prints every value from 9 to 3 with step -1
+
+#print(list(l[:3])) # prints every value from 0 to 2
+
+#print(len(l)) # length of list
+
+#r = list(l) + list(range(0, 10)) #concats lists
+#print(r)
+
+
+
+#url = 'http://google.com'
+#print(url)
+
+#print(url[::-1]) # reverse
+
+#print(url[-4:]) #domain
+
+#print(url[7:]) #without http
+
+#print(url.index('http://'))
+
+#print(url[7:-4])
+
+#print([1] * 10) # creates list of n elements with value 1
