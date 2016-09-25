90fa549ecb8de28b11021826429671c709cd463e
cyperh <cyperh17@gmail.com>
2016-09-25 23:13:22 +0300

    Datetime

--- Dates/script.py
+++ Dates/script.py
@@ -2,8 +2,8 @@
 
 import datetime
 
-d = datetime.date(2016, 7, 24)
-today = datetime.date.today()
+#d = datetime.date(2016, 7, 24)
+#today = datetime.date.today()
 
 #print(d)
 #print(today)
@@ -13,7 +13,7 @@ today = datetime.date.today()
 #print(today.weekday()) #monday - 0; sunday - 6
 #print(today.isoweekday()) #monday - 1; sunday - 7
 
-tdelta = datetime.timedelta(days=7)
+#tdelta = datetime.timedelta(days=7)
 
 # print(today + tdelta)
 # print(today - tdelta)
@@ -21,9 +21,9 @@ tdelta = datetime.timedelta(days=7)
 #date = date + timedelta
 #timedelta = date1 + date2
 
-bday = datetime.date(2017, 5, 9)
+#bday = datetime.date(2017, 5, 9)
 
-till_bday = bday - today
+#till_bday = bday - today
 
 #print(till_bday) #days to birth day
 #print(till_bday.total_seconds()) #in seconds
@@ -34,9 +34,9 @@ till_bday = bday - today
 # print(t)
 # print(t.hour)
 
-dt = datetime.datetime(2016, 7, 26, 12, 30, 45, 100000)
-tdelta_7 = datetime.timedelta(days=7)
-tdelta_12 = datetime.timedelta(hours=12)
+#dt = datetime.datetime(2016, 7, 26, 12, 30, 45, 100000)
+#tdelta_7 = datetime.timedelta(days=7)
+#tdelta_12 = datetime.timedelta(hours=12)
 
 # print(dt + tdelta_7)
 
@@ -48,8 +48,26 @@ tdelta_12 = datetime.timedelta(hours=12)
 # print(dt.year)
 
 
-dt_today = datetime.datetime.today()
-dt_now = datetime.datetime.now()
-dt_utcnow = datetime.datetime.utcnow()
+#dt_today = datetime.datetime.today()
+#dt_now = datetime.datetime.now()
+#dt_utcnow = datetime.datetime.utcnow()
 
-#https://youtu.be/eirjjyP2qcQ?list=PL-osiE80TeTt2d9bfVyTiXJA-UTHn6WwU&t=726
+d_now = datetime.datetime.now()
+
+print(d_now)
+
+d_str = '01.12.2013'
+d = datetime.datetime.strptime(d_str, '%d.%m.%Y')
+
+#strftime - converts Datetime to String
+#strptime - converts String to Datetime
+
+'''
+%d is the day number
+%m is the month number
+%b is the month abbreviation
+%y is the year last two digits
+%Y is the all year
+'''
+
+print(d)
